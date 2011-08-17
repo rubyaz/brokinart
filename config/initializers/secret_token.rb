f7b073e230b7756901a2f438adc7bc497780432e
GavinMcD <gavin@brightmonk.com>
2011-08-16 20:21:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brokinart::Application.config.secret_token = '3c5f547de03de289068e65dca07577bb26d0062d6f5b26c3c62070c23a90f68296447ce334fe7375df5e5de5d758ca476017cee107aa791a57a1262be7994b6f'
