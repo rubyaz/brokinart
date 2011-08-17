b73a417cf3fd8340a47140f3c6ed98813e07db6f
GavinMcD <gavin@brightmonk.com>
2011-08-16 20:49:42 -0700

    Added scaffolding for Product

---
+++ db/migrate/20110817034649_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration
+  def self.up
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.integer :quantity
+      t.date :birth
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :products
+  end
+end
