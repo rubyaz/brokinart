1a8f92275da7751167c13af6ff71aa05b0cfc5cb
Elio Grieco <grieco@egx.com>
2011-09-11 16:34:18 -0700

    Added the ability to add and edit ArtistRoles to Products textually.

--- app/models/artist_role.rb
+++ app/models/artist_role.rb
@@ -1,2 +1,8 @@
 class ArtistRole < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :product
+
+  def name_and_role
+    "#{artist.name} (#{role})"
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :materials
+  has_many :artist_roles
   has_many :artists, :through => :artist_roles
 
   def material_list
@@ -18,5 +19,31 @@ class Product < ActiveRecord::Base
     end
     # need explicitly call on self here or ruby will think we are creating a new local variable
     self.materials = material_list
+  end
+
+  def artist_list
+    artist_roles.collect{|a| a.name_and_role}.join(', ')
+  end
+
+  def artist_list=(artist_string)
+    artist_names = artist_string.split(',')
+    # we are creating a separate list and appending to that since we want to completely replace the artists list rather than just append to it
+    role_list = Array.new
+    artist_names.each do |a|
+      # calling downcase here to normalize the names and prevent duplicates like steel, Steel, STEEL
+      a_string = a.strip.downcase
+      next if a_string.empty?
+      # couldn't decide whether to allow spaces in the role name, they are allowed for now
+      role_match = a_string.match /^([\w ]+?)\s*(?:\(\s*([\w ]+?)\s*\))?$/
+      next if role_match.nil?
+      a_name = role_match[1].strip
+      # 'creator' is the default role for now
+      role = role_match[2] || 'creator'
+      puts "DEBUG: Creating ArtistRole: #{a_name} (#{role})"
+      role_list.push(ArtistRole.new(:artist_id => Artist.find_or_create_by_name(a_name).id, :product_id => self.id, :role => role))
+    end
+    # we are doing this here so we can bail without modification if there are errors
+    self.artists.clear
+    role_list.each{|r| r.save}
   end
 end
